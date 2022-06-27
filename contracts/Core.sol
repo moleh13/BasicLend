897296cd7a8a5901c94fc8753bdf1132f9bdc6a9
moleh13 <molehann@gmail.com>
2022-06-27 18:38:46 +0300

    lending is working

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -5,112 +5,76 @@ import "./IERC20.sol";
 
 contract Core {
 
+    /* ---------------------------- VARIABLES ---------------------------- */
+
     address public WETH;
     address public DAI;
-
-    mapping (address => uint) lendedAmount;
-    mapping (address => uint) borrowedAmount;
-    mapping (address => mapping (address => uint)) borrowedhAmountByUsers;
-    mapping (address => mapping (address => uint)) hAmountsByUsers;
-    mapping (address => uint) exchangeRates;
-    mapping (address => uint) utilizations;
-    mapping (address => uint) borrowMultiplier;
-
     uint public lastUpdatedExchangeRate;
     uint public lastUpdatedBorrowMultiplier;
+    uint public year = 365 * 24 * 60 * 60;
+    uint public passedTime = block.timestamp - lastUpdatedExchangeRate;
+
+    /* ---------------------------- MAPPING ---------------------------- */
+
+    mapping (address => uint) public lendedAmount;
+    mapping (address => uint) public borrowedAmount;
+    mapping (address => uint) public exchangeRates;
+    mapping (address => uint) public utilizations;
+    mapping (address => uint) public borrowMultiplier;
+    mapping (address => mapping (address => uint)) public borrowedhAmountByUsers;
+    mapping (address => mapping (address => uint)) public hAmountsByUsers;
+
+    /* ---------------------------- EVENTS ---------------------------- */
 
     event Log(address addr, uint amount);
 
-    address public owner;
+    /* ---------------------------- CONSTRUCTOR ---------------------------- */
 
     constructor(address _WETH, address _DAI) {
         WETH = _WETH;
         DAI = _DAI;
-        owner = msg.sender;
-        borrowMultiplier[WETH] = 1;
-        borrowMultiplier[DAI] = 1;
         exchangeRates[WETH] = 1e18;
         exchangeRates[DAI] = 1e18;
+        lendedAmount[WETH] = 1;
+        borrowedAmount[WETH] = 1e18;
         lastUpdatedExchangeRate = block.timestamp;
         lastUpdatedBorrowMultiplier = block.timestamp;
-        lendedAmount[WETH] = 3;
-        borrowedAmount[WETH] = 1;
     }
+    
+    /* ---------------------------- LENDING ---------------------------- */
 
     function lend(address _market, uint _amount) external {
         updateExchangeRate(_market);
-        lendedAmount[_market] += _amount;
-        IERC20(_market).approve(address(this), _amount);
-        IERC20(_market).transferFrom(msg.sender, address(this), _amount);
-        hAmountsByUsers[msg.sender][_market] += (_amount * 1e36) / exchangeRates[_market] ;
+
+        lendedAmount[_market] += _amount * 1e18;
+
+        IERC20(_market).transferFrom(msg.sender, address(this), _amount * 1e18);
+
+        hAmountsByUsers[msg.sender][_market] += (_amount * 1e36) / (exchangeRates[_market]);
+
         updateUtilization(_market);
+
+        emit Log(msg.sender, hAmountsByUsers[msg.sender][_market] * exchangeRates[_market] / 1e18);
     }
 
-    function redeem(address _market, uint _amount) external {
-        updateExchangeRate(_market);
-        require(hAmountsByUsers[msg.sender][_market] * exchangeRates[_market] >= _amount);
-        hAmountsByUsers[msg.sender][_market] -= _amount / exchangeRates[_market];
-        IERC20(_market).transfer(msg.sender, _amount);
-        lendedAmount[_market] -= _amount;
-        updateUtilization(_market);
-    }
+    /* ---------------------------- UPDATE PARTS ---------------------------- */
 
-    function borrow(address _market, uint _amount) external {
-        updateBorrowMultiplier(_market);
-        borrowedhAmountByUsers[msg.sender][_market] += _amount / borrowMultiplier[_market];
-        IERC20(_market).transfer(msg.sender, _amount);
+    function updateExchangeRate(address _market) public {
+        exchangeRates[_market] += (passedTime) * (utilizations[_market]) / (year) * exchangeRates[_market] / 1e18;
+        
         updateUtilization(_market);
-    }
 
-    function updateExchangeRate(address _market) internal {
-        exchangeRates[_market] = exchangeRates[_market] + (block.timestamp - lastUpdatedExchangeRate) * (utilizations[_market]) /
-        (365 * 24 * 60 * 60) * exchangeRates[_market] / 1e18;
-        updateUtilization(_market);
         lastUpdatedExchangeRate = block.timestamp;
+
+        emit Log(_market, exchangeRates[_market]);
     }
 
     function updateUtilization(address _market) public {
         utilizations[_market] = (borrowedAmount[_market] * 1e18) / lendedAmount[_market];
-    }
-
-    function updateBorrowMultiplier(address _market) public {
-        borrowMultiplier[_market] = borrowMultiplier[_market] + (block.timestamp - lastUpdatedBorrowMultiplier) * (utilizations[_market]) /
-        (365 * 24 * 60 * 60) * borrowMultiplier[_market] / 1e18;
-        updateUtilization(_market);
-        lastUpdatedBorrowMultiplier = block.timestamp;
-    }
-
-    function getLendedAmount(address _market) public returns (uint) {
-        updateExchangeRate(_market);
-
-        emit Log(msg.sender, hAmountsByUsers[msg.sender][_market] * exchangeRates[_market]);
-
-        return hAmountsByUsers[msg.sender][_market] * exchangeRates[_market];
-    }
-
-    function getBorrowedAmount(address _market) public returns (uint) {
-        updateBorrowMultiplier(_market);
-        
-        emit Log(msg.sender, borrowedhAmountByUsers[msg.sender][_market] * borrowMultiplier[_market]);
-
-        return borrowedhAmountByUsers[msg.sender][_market] * borrowMultiplier[_market];
-    }
-
-    function getUtilization(address _market) public returns (uint) {
-        updateUtilization(_market);
 
         emit Log(_market, utilizations[_market]);
-
-        return utilizations[_market];
     }
 
-    function getExchangeRate(address _market) public returns (uint) {
-        updateExchangeRate(_market);
-
-        emit Log(_market, exchangeRates[_market]);
-
-        return exchangeRates[_market];
-    }
 }
 
 // 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8, 0xd9145CCE52D386f254917e481eB44e9943F39138
